bf4e49b9532f4718ccc2048a2310e78073d752ef
PowerAngel <Jonatan.Berg@live.com>
2015-06-16 18:51:04 +0200

    AddPlayer bör vara klart

--- TeamCreator/app/src/main/java/com/example/golvmopp/teamcreator/AddPlayers.java
+++ TeamCreator/app/src/main/java/com/example/golvmopp/teamcreator/AddPlayers.java
@@ -1,16 +1,17 @@
 package com.example.golvmopp.teamcreator;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,28 +19,85 @@ import java.util.ArrayList;
 public class AddPlayers extends ActionBarActivity {
 
     private ArrayAdapter<String> adapter;
-    private Spinner spinner; //Kanske inte behövs
     ArrayList<Player> PlayersArray = new ArrayList<Player>();
+    ArrayList<Player> ChosenArray = new ArrayList<Player>();
+    String myLogTag = "MyTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
         //searchView searchView_players;
-        final TextView textView_players = (TextView) this.findViewById(R.id.textView_Players);
-        Button btn_AddName = (Button) this.findViewById(R.id.btn_AddName);
         final EditText editText_name = (EditText) this.findViewById(R.id.editText_name);
-        final ListView liteView_Players = (ListView) this.findViewById(R.id.listView_Players);
+        final ListView listView_Players = (ListView) this.findViewById(R.id.listView_Players);
+        listView_Players.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listView_Players.setItemsCanFocus(false);
 
-        btn_AddName.setOnClickListener(new View.OnClickListener() {
+        ImageButton imageButton_add = (ImageButton) this.findViewById(R.id.imageButton_AddName);
+        ImageButton imageButton_Done = (ImageButton) this.findViewById(R.id.imageButton_Done);
+
+        final ArrayList<String> NameArray = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, NameArray);
+        listView_Players.setAdapter(adapter);
+
+        listView_Players.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                if(editText_name != null)
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                try
+                {
+                    int check = 0;
+                    for(int i = 0; i < ChosenArray.size(); i++)
+                    {
+                        if(ChosenArray.get(i).getName().equals(PlayersArray.get(position).getName()))
+                        {
+                            ChosenArray.remove(i);
+                            check = 1;
+                        }
+                    }
+                    if(check == 0)
+                    {
+                        ChosenArray.add(PlayersArray.get(position));
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Log.v(myLogTag, "Nu gick något fel" + e);
+                }
+            }
+
+        });
+
+        imageButton_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if (editText_name != null)
                 {
                     Player player = new Player(editText_name.getText().toString(), 1);
                     PlayersArray.add(player);
-                    //textView_players.append("\n" + player.getName());
-                    adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, PlayersArray);
+                    NameArray.add(player.getName());
+                    listView_Players.setAdapter(adapter);
+                    editText_name.setText("");
+                }
+            }
+        });
+
+        imageButton_Done.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                try
+                {
+                    //Intent intent = new Intent(getApplicationContext(), CreateTeams.class);
+                    //intent.putParcelableArrayListExtra("ChosenArray", ChosenArray);
+                    //startActivity(intent);
+                }
+                catch(Exception e)
+                {
+                    Log.v(myLogTag, "Nu gick något fel");
                 }
             }
         });
--- TeamCreator/app/src/main/java/com/example/golvmopp/teamcreator/Player.java
+++ TeamCreator/app/src/main/java/com/example/golvmopp/teamcreator/Player.java
@@ -1,9 +1,12 @@
 package com.example.golvmopp.teamcreator;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Golvmopp on 2015-06-16.
  */
-public class Player
+public class Player implements Parcelable
 {
     private String name;
     private double skill;
@@ -25,4 +28,34 @@ public class Player
 
     public double getSkill(){return skill;}
     public void setSkill(){this.skill = skill;}
+
+    @Override
+    public int describeContents()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    private Player(Parcel in)
+    {
+        name = in.readString();
+        skill = in.readDouble();
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags)
+    {
+        out.writeString(name);
+        out.writeDouble(skill);
+    }
+
+    public static final Parcelable.Creator<Player> CREATOR = new Parcelable.Creator<Player>() {
+        public Player createFromParcel(Parcel in) {
+            return new Player(in);
+        }
+
+        public Player[] newArray(int size) {
+            return new Player[size];
+        }
+    };
 }
