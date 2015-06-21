4ac8927a33621aead3c637c2c9f042ed5a4bac35
PowerAngel <Jonatan.Berg@live.com>
2015-06-21 11:46:10 +0200

    Nu kraschar den inte vid uppstart när det inte finns några spelare inlagda

--- TeamCreator/app/src/main/java/com/example/golvmopp/teamcreator/AddPlayers.java
+++ TeamCreator/app/src/main/java/com/example/golvmopp/teamcreator/AddPlayers.java
@@ -40,9 +40,9 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
     private String FILE_NAME = "Players.txt";
     ListView listView_Players = null;
     SearchView searchView_players;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
 
@@ -54,37 +54,26 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
         fixListView();
     }
 
-    public void buttonDone()
-    {
+    public void buttonDone() {
         ImageButton imageButton_Done = (ImageButton) this.findViewById(R.id.imageButton_Done);
-        imageButton_Done.setOnClickListener(new View.OnClickListener()
-        {
+        imageButton_Done.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                if(ChosenArray.size() > 0)
-                {
-                    try
-                    {
+            public void onClick(View v) {
+                if (ChosenArray.size() > 0) {
+                    try {
                         Intent intent = new Intent(getApplicationContext(), CreateTeams.class);
                         intent.putParcelableArrayListExtra("ChosenArray", ChosenArray);
                         startActivity(intent);
-                    }
-                    catch (Exception e)
-                    {
+                    } catch (Exception e) {
                         Log.v(myLogTag, "Nu gick n�got fel i intenten: " + e);
                     }
-                }
-                else
-                {
+                } else {
                     new AlertDialog.Builder(AddPlayers.this)
                             .setTitle("No player selected")
                             .setMessage("You have to select at least one player to continue")
-                            .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener()
-                            {
+                            .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                                 @Override
-                                public void onClick(DialogInterface dialog, int which)
-                                {
+                                public void onClick(DialogInterface dialog, int which) {
 
                                 }
                             }).show();
@@ -93,73 +82,54 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
         });
     }
 
-    public void buttonAdd()
-    {
+    public void buttonAdd() {
         final EditText editText_name = (EditText) this.findViewById(R.id.editText_name);
         ImageButton imageButton_add = (ImageButton) this.findViewById(R.id.imageButton_AddName);
-        imageButton_add.setOnClickListener(new View.OnClickListener()
-        {
+        imageButton_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editText_name.getText().length() != 0)
-                {
+                if (editText_name.getText().length() != 0) {
                     int check = 0;
 
-                    for(int i = 0; i < PlayersArray.size(); i++)
-                    {
-                        if(PlayersArray.get(i).getName().equals(editText_name.getText().toString()))
-                        {
+                    for (int i = 0; i < PlayersArray.size(); i++) {
+                        if (PlayersArray.get(i).getName().equals(editText_name.getText().toString())) {
                             check = 1;
                         }
                     }
 
-                    if(check == 0)
-                    {
+                    if (check == 0) {
                         Player player = new Player(editText_name.getText().toString(), 1);
                         PlayersArray.add(player);
                         NameArray.add(player.getName());
                         ((BaseAdapter) listView_Players.getAdapter()).notifyDataSetChanged();
                         editText_name.setText("");
-                    }
-                    else
-                    {
+                    } else {
                         editText_name.setText("");
                         Log.v(myLogTag, "Namnet finns redan");
                     }
-                }
-                else
-                {
+                } else {
                     Log.v(myLogTag, "Inget namn ifyllt");
                 }
             }
         });
     }
 
-    public void buttonRemove()
-    {
+    public void buttonRemove() {
         ImageButton imageButton_remove = (ImageButton) this.findViewById(R.id.imageButton_remove);
-        imageButton_remove.setOnClickListener(new View.OnClickListener()
-        {
+        imageButton_remove.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                for (int i = 0; i < ChosenArray.size(); i++)
-                {
-                    for (int k = 0; k < PlayersArray.size(); k++)
-                    {
-                        if(ChosenArray.get(i).getName().equals(PlayersArray.get(k).getName()))
-                        {
+            public void onClick(View v) {
+                for (int i = 0; i < ChosenArray.size(); i++) {
+                    for (int k = 0; k < PlayersArray.size(); k++) {
+                        if (ChosenArray.get(i).getName().equals(PlayersArray.get(k).getName())) {
                             PlayersArray.remove(k);
                             int removeName = -1;
-                            for (int j = 0; j < NameArray.size(); j++)
-                            {
-                                if(NameArray.get(j).toString().equals(ChosenArray.get(i).getName()))
-                                {
+                            for (int j = 0; j < NameArray.size(); j++) {
+                                if (NameArray.get(j).toString().equals(ChosenArray.get(i).getName())) {
                                     removeName = j;
                                 }
                             }
-                            if(removeName != -1)
-                            {
+                            if (removeName != -1) {
                                 NameArray.remove(removeName);
                             }
                         }
@@ -167,8 +137,7 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
                 }
                 ChosenArray = new ArrayList<Player>();
                 ((BaseAdapter) listView_Players.getAdapter()).notifyDataSetChanged();
-                for(int i = 0; i < listView_Players.getCount(); i++)
-                {
+                for (int i = 0; i < listView_Players.getCount(); i++) {
                     listView_Players.setItemChecked(i, false);
                 }
 
@@ -176,8 +145,7 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
         });
     }
 
-    public void fixSearchView()
-    {
+    public void fixSearchView() {
         searchView_players = (SearchView) this.findViewById(R.id.searchView_players);
         searchView_players.setIconifiedByDefault(false);
         searchView_players.setOnQueryTextListener(this);
@@ -185,8 +153,8 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
         searchView_players.setQueryHint("Search here");
 
     }
-    public void fixListView()
-    {
+
+    public void fixListView() {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, NameArray);
         listView_Players = (ListView) this.findViewById(R.id.listView_Players);
         listView_Players.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
@@ -194,34 +162,26 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
         listView_Players.setTextFilterEnabled(true);
         listView_Players.setAdapter(adapter);
 
-        for(int i = 0; i < listView_Players.getCount(); i++)
-        {
+        for (int i = 0; i < listView_Players.getCount(); i++) {
             listView_Players.setItemChecked(i, false);
         }
 
         listView_Players.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                try
-                {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                try {
                     int check = 0;
-                    for (int i = 0; i < ChosenArray.size(); i++)
-                    {
-                        if (ChosenArray.get(i).getName().equals(PlayersArray.get(position).getName()))
-                        {
+                    for (int i = 0; i < ChosenArray.size(); i++) {
+                        if (ChosenArray.get(i).getName().equals(PlayersArray.get(position).getName())) {
                             ChosenArray.remove(i);
                             check = 1;
                         }
                     }
-                    if (check == 0)
-                    {
+                    if (check == 0) {
                         ChosenArray.add(PlayersArray.get(position));
                     }
 
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     Log.v(myLogTag, "Nu gick n�got fel" + e);
 
                     Log.v(myLogTag, "Nu gick n�got fel i markeringen av players: " + e);
@@ -253,90 +213,73 @@ public class AddPlayers extends ActionBarActivity implements SearchView.OnQueryT
         return super.onOptionsItemSelected(item);
     }
 
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
         saveArrayList();
     }
 
-    public void importArrayList()
-    {
+    public void importArrayList() {
         PlayersArray = new ArrayList<Player>();
         NameArray = new ArrayList<String>();
-        try
-        {
+        try {
             FileInputStream fis = openFileInput(FILE_NAME);
             Scanner scanner = new Scanner(fis);
             String[] tokens;
-            while(scanner.hasNext())
-            {
+            while (scanner.hasNext()) {
                 String text = scanner.nextLine();
                 Log.v(myLogTag, "text.Length(): " + text.length());
-                if(text != null && !text.isEmpty() && text.length() != 1 && text != "null")
-                {
-                    tokens = text.split(",");
+
+                tokens = text.split(",");
+                if (tokens.length != 1) {
                     Log.v(myLogTag, "text: " + text);
-                    Log.v(myLogTag,"tokens[0]: " + tokens[0] + " tokens[1]: " + tokens[1]);
+                    Log.v(myLogTag, "tokens[0]: " + tokens[0] + " tokens[1]: " + tokens[1]);
 
                     Player player = new Player(tokens[0], Double.parseDouble(tokens[1]));
                     PlayersArray.add(player);
                     NameArray.add(player.getName());
                     Log.v(myLogTag, "player.getname: " + player.getName() + " || player.getskill: " + player.getSkill());
                 }
+
             }
             scanner.close();
-        }
-        catch(FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             Log.e(myLogTag, e.getMessage(), e);
         }
     }
-    public void saveArrayList()
-    {
-        try
-        {
+
+    public void saveArrayList() {
+        try {
             FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
             PrintWriter pw = new PrintWriter(fos);
             String text = null;
 
-            for(int i = 0; i < PlayersArray.size(); i++)
-            {
-                if(i == 0)
-                {
+            for (int i = 0; i < PlayersArray.size(); i++) {
+                if (i == 0) {
                     text = PlayersArray.get(i).getName() + "," + String.valueOf(PlayersArray.get(i).getSkill() + "\n");
-                }
-                else
-                {
+                } else {
                     text = text + PlayersArray.get(i).getName() + "," + String.valueOf(PlayersArray.get(i).getSkill() + "\n");
                 }
 
             }
-            Log.v(myLogTag,"text: " + text);
+            Log.v(myLogTag, "text: " + text);
             pw.println(text);
             pw.close();
-        }
-        catch(FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             Log.e(myLogTag, e.getMessage(), e);
         }
     }
 
     @Override
-    public boolean onQueryTextSubmit(String query)
-    {
+    public boolean onQueryTextSubmit(String query) {
 
         return false;
     }
 
     @Override
-    public boolean onQueryTextChange(String newText)
-    {
-        if(TextUtils.isEmpty(newText))
-        {
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
             listView_Players.clearTextFilter();
-        }
-        else
-        {
+        } else {
             listView_Players.setFilterText(newText.toString());
         }
         return true;
